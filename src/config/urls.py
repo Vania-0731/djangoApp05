bcaec679cf8e7961609d1a66e8c601c858df26dc
Vania <sonalysifuentes@gmail.com>
2025-04-16 14:46:58 -0500

    configuratiosn of urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('library.urls'))
+    path('api/', include('library.urls')),
+    path('circulation/', include('circulation.urls')),
 ]
 
 if settings.DEBUG:
