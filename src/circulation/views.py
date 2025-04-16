e828a254064ae64d0859e91a5ce6c13eee0d42f1
Vania <sonalysifuentes@gmail.com>
2025-04-16 14:43:42 -0500

    creation of views, forms and manage of panel admin

--- src/circulation/views.py
+++ src/circulation/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, BookCopy, Loan, Reservation
+from .forms import LoanForm, ReservationForm
 
-# Create your views here.
+
+def loan_book(request):
+    if request.method == "POST":
+        form = LoanForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('loan_success')
+    else:
+        form = LoanForm()
+    return render(request, 'circulation/loan_book.html', {'form': form})
+
+def reserve_book(request):
+    if request.method == "POST":
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('reservation_success')
+    else:
+        form = ReservationForm()
+    return render(request, 'circulation/reserve_book.html', {'form': form})
